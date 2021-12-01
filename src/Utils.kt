eadc672d3908e5c2e888f7bc7165ca9d05b83aa4
Andrei Kavaleuski <spyroid@gmail.com>
2021-12-01 10:08:19 +0100

    /

--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,5 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+fun strToIntSeq(list: List<String>): Sequence<Int> = list.asSequence().map { it.toInt() }
--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,21 +1,34 @@
 package day01
 
 import readInput
+import strToIntSeq
 
 fun main() {
+
+    fun doCount(seq: Sequence<Int>): Int {
+        var marker = Int.MAX_VALUE
+        return seq.map {
+            val res = if (it > marker) 1 else 0
+            marker = it
+            return@map res
+        }.sum()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return doCount(strToIntSeq(input))
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val seq = IntRange(0, input.size -3).asSequence()
+            .map { input[it].toInt() + input[it + 1].toInt() + input[it + 2].toInt() }
+
+        return doCount(seq)
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("day01/test01")
-    check(part1(testInput) == 1)
+    check(part1(readInput("day01/test01")) == 7)
+    check(part2(readInput("day01/test01")) == 5)
 
-    val input = readInput("day01/input01")
-    println(part1(input))
-    println(part2(input))
+    println("--------------------------------")
+    println(part1(readInput("day01/input01")))
+    println(part2(readInput("day01/input01")))
 }
