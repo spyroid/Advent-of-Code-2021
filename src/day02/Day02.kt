701cabd51f45922c8df6035a2d3a63621c920da6
Andrei Kavaleuski <spyroid@gmail.com>
2021-12-02 12:59:19 +0100

    ..

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -1,23 +1,54 @@
 package day02
 
-import readToIntSeq
+import readInput
 
 fun main() {
 
-    fun part1(seq: Sequence<Int>): Long {
-        return 0
+    data class Command(val op: String, val value: Int)
+
+    fun part1(seq: Sequence<Command>): Int {
+        var depth = 0
+        var horizontal = 0
+        seq.forEach {
+            when (it.op) {
+                "forward" -> horizontal += it.value
+                "down" -> depth += it.value
+                "up" -> depth -= it.value
+            }
+        }
+        return depth * horizontal
     }
 
-    fun part2(seq: Sequence<Int>): Long {
-        return 0
+    fun part2(seq: Sequence<Command>): Int {
+        var depth = 0
+        var horizontal = 0
+        var aim = 0
+        seq.forEach {
+            when (it.op) {
+                "forward" -> {
+                    horizontal += it.value
+                    depth += it.value * aim
+                }
+                "down" -> aim += it.value
+                "up" -> aim -= it.value
+            }
+        }
+        return depth * horizontal
     }
 
-    val testSeq = readToIntSeq("day02/test")
-    check(part1(testSeq) == 7L)
-    check(part2(testSeq) == 5L)
+    fun mapCommand(str: String): Command {
+        val parts = str.split(" ")
+        return Command(parts[0], parts[1].toInt())
+    }
 
-    println("--------------------------------")
-    val seq = readToIntSeq("day02/input")
-    println(part1(seq))
-    println(part2(seq))
+    val testSeq = readInput("day02/test").asSequence().map { mapCommand(it) }
+    val seq = readInput("day02/input").asSequence().map { mapCommand(it) }
+
+    val res1 = part1(testSeq)
+    check(res1 == 150) { "Expected 150 but got $res1" }
+    println("Part1: " + part1(seq))
+
+    val res2 = part2(testSeq)
+    check(res2 == 900) { "Expected 900 but got $res2" }
+    println("Part2: " + part2(seq))
 }
