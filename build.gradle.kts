7dccfdd32916c554368cb73c09a4d194539c36f2
Andrei Kavaleuski <spyroid@gmail.com>
2021-12-14 23:55:33 +0100

    @....

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.6.10"
 }
 
 repositories {
@@ -7,10 +7,15 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC2")
     implementation("com.github.ajalt.mordant:mordant:2.0.0-beta2")
 //    implementation("org.jetbrains.kotlinx:multik-api:0.1.1")
 //    implementation("org.jetbrains.kotlinx:multik-default:0.1.1")
+}
+
+configure<JavaPluginExtension> {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks {
@@ -18,9 +23,5 @@ tasks {
         main {
             java.srcDirs("src")
         }
-    }
-
-    wrapper {
-        gradleVersion = "7.3"
     }
 }
