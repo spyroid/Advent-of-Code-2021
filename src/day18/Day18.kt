961d446d07ba4c9650d470b59996456a8b4dc9c2
Andrei Kavaleuski <spyroid@gmail.com>
2021-12-20 22:51:57 +0100

    @@

---
+++ src/day18/Day18.kt
@@ -0,0 +1,12 @@
+package day18
+
+import readInput
+
+
+fun main() {
+
+    val testData = readInput("day18/test")
+    val inputData = readInput("day18/input")
+
+}
+
---
+++ src/day19/Day19.kt
@@ -0,0 +1,12 @@
+package day19
+
+import readInput
+
+fun main() {
+
+
+    val testData = readInput("day19/test")
+    val inputData = readInput("day19/input")
+
+}
+
