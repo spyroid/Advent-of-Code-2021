109d9f11c37d3c6dfe9472b21d87bcb50aeefe40
Andrei Kavaleuski <spyroid@gmail.com>
2021-12-27 21:50:09 +0100

    /

--- src/day18/Day18.kt
+++ src/day18/Day18.kt
@@ -4,37 +4,45 @@ import readInput
 import kotlin.system.measureTimeMillis
 
 data class Node(var value: Int? = null, var left: Node? = null, var right: Node? = null) {
-    fun find3th(): Node? {
-        fun deeper(level: Int, node: Node?): Node? {
-            if (level == 3 || node == null) return node
-            return deeper(level + 1, node)
+    override fun toString(): String = if (value != null) value.toString() else "[$left,$right]"
+}
+
+fun parseLine(line: String): Node {
+    var idx = 0
+    fun parse(): Node {
+        if (line[idx] == '[') {
+            idx++
+            val l = parse()
+            idx++ // ,
+            val r = parse()
+            idx++ // ]
+            return Node(null, l, r)
         }
-        var level = 0
-        val l = deeper(1, left)
-        val r = deeper(1, right)
-        return null
+        val start = idx
+        while (line[idx] in '0'..'9') idx++
+        return Node(line.substring(start, idx).toInt())
     }
+    return parse()
 }
 
 fun main() {
+    val input = readInput("day18/test")
 
-    fun part1(input: List<String>): Int {
+    var root: Node? = null
+    for (line in input) {
+        if (root == null) {
+            root = parseLine(line)
+            continue
+        }
+        root = Node(null, root, parseLine(line))
 
-        val root = Node(null, Node(1), Node(2))
 
-        return 0
     }
 
-    fun part2(input: List<String>) = input.size
-
-
-    val testData = readInput("day18/test")
-    val inputData = readInput("day18/input")
-
-    check(part1(testData) == 0)
-
-    measureTimeMillis { print("⭐️ Part1: ${part1(inputData)}") }.also { time -> println(" in $time ms") }
-    measureTimeMillis { print("⭐️ Part2: ${part2(inputData)}") }.also { time -> println(" in $time ms") }
+//    check(part1(testData) == 0)
+//
+//    measureTimeMillis { print("⭐️ Part1: ${part1(inputData)}") }.also { time -> println(" in $time ms") }
+//    measureTimeMillis { print("⭐️ Part2: ${part2(inputData)}") }.also { time -> println(" in $time ms") }
 
 }
 
