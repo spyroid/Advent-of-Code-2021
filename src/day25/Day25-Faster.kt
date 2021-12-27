262d340ec280c0cba2003dff44b62c5fac382567
Andrei Kavaleuski <spyroid@gmail.com>
2021-12-27 02:33:36 +0100

    @@&

---
+++ src/day25/Day25-Faster.kt
@@ -0,0 +1,61 @@
+package day25
+
+import com.github.ajalt.mordant.rendering.TextColors.*
+import com.github.ajalt.mordant.terminal.Terminal
+import readInput
+import kotlin.system.measureTimeMillis
+
+fun main() {
+
+    val input = readInput("day25/test3")
+    val height = input.size
+    val width = input.first().length
+    var area = Array(height) { Array(width) { '.' } }
+    input.forEachIndexed { y, line -> line.forEachIndexed { x, c -> area[y][x] = c } }
+
+    fun move(ch: Char, dx: Int, dy: Int): Boolean {
+        var moved = false
+        val area1 = Array(height) { Array(width) { '.' } }
+        for (y in 0 until height) for (x in 0 until width) area1[y][x] = area[y][x]
+
+        for (y in 0 until height) for (x in 0 until width) {
+            if (area[y][x] != ch) continue
+
+            val xx = (x + dx) % width
+            val yy = (y + dy) % height
+            val cc = area[yy][xx]
+            if (cc == '.') {
+                area1[yy][xx] = ch
+                area1[y][x] = '.'
+                moved = true
+            } else {
+                area1[y][x] = ch
+            }
+        }
+        area = area1
+        return moved
+    }
+
+    val term = Terminal()
+    val colors = mapOf('.' to gray, '>' to brightRed, 'v' to brightBlue)
+    fun printArea() {
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                val c = area[y][x]
+                term.print("${colors[c]?.let { it(c.toString()) }}")
+            }
+            term.println()
+        }
+    }
+
+    var count = 0
+    measureTimeMillis {
+        printArea().also { println() }
+        do {
+            count++
+            val r1 = move('>', 1, 0)
+            val r2 = move('v', 0, 1)
+        } while (r1 || r2)
+        printArea()
+    }.also { println("⭐️ Part1: $count in $it ms") }
+}
